d27cd7e5bd91e80817134c13f25d5dd0f34243cf
Shmukers <platonflaviu@yahoo.com>
2015-01-09 02:55:18 +0200

    fixed several mistakes for gyro init

--- RCXSERVER/static/gyro.js
+++ RCXSERVER/static/gyro.js
@@ -1,27 +1,32 @@
-// sets up the events/*
-if ((window.DeviceMotionEvent) {
-    window.addEventListener('devicemotion', deviceMotionHandler, false);
+  init();
 
-}
+  function init() {
+      if ((window.DeviceMotionEvent) || ('listenForDeviceMovement' in window)) {
+          window.addEventListener('devicemotion', deviceMotionHandler, false);
+      } else {
+          console.log("Not supported on your device or browser.  Sorry.");
+      }
+  }
 
-function deviceMotionHandler(eventData){
-    var acceleration = eventData.acceleration;
-    issueCommand("x",acceleration.x);
-    issueCommand("y",acceleration.y);
-    issueCommand("z",acceleration.z);
-}
+  function deviceMotionHandler(eventData) {
+      var acceleration = eventData.acceleration;
+      issueCommand("x", acceleration.x);
+      issueCommand("y", acceleration.y);
+      issueCommand("z", acceleration.z);
+  }
 
-// requests from the API
-function issueCommand(axis, value) {
-    var client = new XMLHttpRequest();
+   // requests from the API
+  function issueCommand(axis, value) {
+      var client = new XMLHttpRequest();
 
-    client.open("GET", "http://" + window.location.hostname + ":3000/gyro/" + axis + "/" + value, true);
-    client.setRequestHeader("Connection", "close");
-    client.send();
+      client.open("GET", "http://" + window.location.hostname + ":3000/gyro/" + axis + "/" + value, true);
+      client.setRequestHeader("Connection", "close");
+      client.send();
 
-    client.onreadystatechange = function () {
-        if (client.readyState == 4 && client.status == 200) {
-            console.log(client.responseText);
-        }
-    }
-}
+      client.onreadystatechange = function () {
+          if (client.readyState == 4 && client.status == 200) {
+              console.log(client.responseText);
+          }
+      }
+  }
+
